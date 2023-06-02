8592e6fd29f9c2dfe3f8905effb55061529a1606
haley <warleyhacker@hotmail.com.com>
2023-06-02 02:23:31 -0300

    save

---
+++ App.js
@@ -0,0 +1,72 @@
+import { StyleSheet, View,PermissionsAndroid } from 'react-native';
+import WebView from 'react-native-webview';
+import Constants from 'expo-constants'
+
+export default function App() {
+
+  const functions_before =
+  `
+  `;
+
+  PermissionsAndroid.request(
+    PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    {
+      title: 'Location Access Permission',
+      message: 'We would like to use your location',
+      buttonPositive: 'Okay'
+    }
+  );
+
+  console.log(Constants.deviceName)
+
+
+  // onBackButtonPressAndroid = (e) => {   
+  //   if (this.state.canGoBack) {
+  //     if (this.webview.goBack) {
+  //       this.webview.goBack();
+  //     }
+
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // };
+
+  // () => props.navigation?.openDrawer()
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.top}></View>
+
+      <WebView
+        geolocationEnabled={true}
+        javaScriptEnabled={true}
+        allowFileAccess={true}
+        domStorageEnabled={true}
+        mediaPlaybackRequiresUserAction={false}
+        onTouchEnd={function(e) {
+          console.log(e)
+        }}
+
+        originWhitelist={'https://ninosflores.codehalley.com'}
+        source={{ uri: 'https://ninosflores.codehalley.com', baseUrl: 'https://ninosflores.codehalley.com' }}
+
+        injectedJavaScriptBeforeContentLoaded={`document.addEventListener("DOMContentLoaded", function(){${functions_before}})`}
+        style={styles.webview}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  top: {
+    backgroundColor:"#3170d7",
+    height: Constants.statusBarHeight ?? 0
+  },
+  container: {
+    flex: 1
+  },
+  webview: {
+    flex: 1
+  },
+});
